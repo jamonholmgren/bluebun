26b0bb4418c45af52a5ff499c44426f13ab32e41
Jamon Holmgren <jamonholmgren@gmail.com>
2023-09-23 00:08:46 -0700

    Added temporary perf test

---
+++ cli/commands/perf.ts
@@ -0,0 +1,19 @@
+import { Toolbox, print, ask } from "bluebun"
+
+// run this with `bun run --inspect ./bluebun perf` to see the inspector
+export default {
+  name: "perf",
+  description: "Runs stress test for inspector",
+  run: async (toolbox: Toolbox) => {
+    // long running perf test
+    print("Running perf tests...")
+    for (let i = 0; i < 100000000; i++) {
+      // load a file
+      const file = await Bun.file("package.json").json()
+      // look at the 3rd character
+      if (file.name[2] === "e") {
+        // do nothing
+      }
+    }
+  },
+}
